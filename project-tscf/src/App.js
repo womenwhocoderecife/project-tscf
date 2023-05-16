e06c180184e34d9c94f4719a6a625e6621a0a214
Eduarda <duda.eduarda.ec@gmail.com>
2023-05-16 17:35:50 -0300

    Retirando botão do app.js usado para visualização

--- project-tscf/src/App.js
+++ project-tscf/src/App.js
@@ -3,16 +3,6 @@ import Button from './components/Button';
 
 
 function App() {
-  const handleClick = () => {
-    console.log('Botão clicado!');
-  };
-
-  return (
-    <div>
-      <h1>O MEU TA ASSIM: </h1>
-      <Button onClick={handleClick} text="Donate" />
-    </div>
-  );
 }
 
 export default App;
